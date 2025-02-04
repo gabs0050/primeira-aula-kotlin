829355df3e3fed69a7d02087c153724fe5b51d0d
GABRIEL SOUZA COSTA <24122537@dsn127m1242375.cfp127aluno.local>
2025-02-04 09:28:02 -0300

    first commit

---
+++ src/main/kotlin/br/senai/sp/jandira/imc/App.kt
@@ -0,0 +1,44 @@
+package br.senai.sp.jandira.imc
+
+import br.senai.sp.jandira.imc.model.Medico
+import br.senai.sp.jandira.imc.model.Paciente
+import java.time.LocalDate
+
+fun main() {
+
+    // Criar o paciente Ana Maria
+    var paciente1 = Paciente()
+    paciente1.id = 1
+    paciente1.nome = "Ana Maria"
+    paciente1.email = "ana@gmail.com"
+    paciente1.altura =  1.67
+    paciente1.peso = 40
+    paciente1.dataNascimento = LocalDate.of(2001, 8, 13)
+    paciente1.calcularImc()
+    paciente1.classificarImc()
+    paciente1.mostrarDadosBiometricos()
+
+    // Criar o paciente Pedro de Oliveira
+    var paciente2 = Paciente()
+    paciente2.id = 2
+    paciente2.nome = "Gabriel Souza Costa"
+    paciente2.email = "souzacostagabriel37@gmail.com"
+    paciente2.altura = 1.68
+    paciente2.peso = 66
+    paciente2.dataNascimento = LocalDate.of(2007, 11, 5)
+    paciente2.calcularImc()
+    paciente2.classificarImc()
+    paciente2.mostrarDadosBiometricos()
+
+    // Criar um objeto médico
+    var medico1 = Medico()
+    var medico2 = Medico()
+
+    medico1.id = 1
+    medico1.nome = "Roberta da Silva"
+    medico1.email = "roberta@hospital.com"
+    medico1.especialidades.add("Nutricionista")
+    medico1.especialidades.add("Gastroenterologista")
+    medico1.exibirFichaMedico()
+}
+
---
+++ src/main/kotlin/br/senai/sp/jandira/imc/model/Medico.kt
@@ -0,0 +1,22 @@
+package br.senai.sp.jandira.imc.model
+
+class Medico {
+
+    var id: Int = 0
+    var nome: String = ""
+    var especialidades: ArrayList<String> = ArrayList<String>()
+    var email: String = ""
+
+    fun exibirFichaMedico(){
+        println("-------------------------")
+        println("FICHA DO MÉDICO")
+        println("-------------------------")
+        println("NOME: $nome")
+        println("E-MAIL: $email")
+        println("Especialidades do médico:")
+        for (especialidades in especialidades){
+            println(" -> $especialidades")
+        }
+        println("=========================")
+    }
+}
